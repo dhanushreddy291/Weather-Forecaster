6ed689ba979a48ab95130f6c3961986f26871136
Dhanush Reddy <29dhanushreddy@gmail.com>
2022-01-08 17:04:20 +0530

    Updating

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,10 +1,5 @@
 import * as React from "react";
 import "./styles/weather.css";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
 
 export default function weather({ weatherData }) {
   const date = new Date(weatherData.location.localtime);

27af0800e08913149ff28062a9525d23a6eed6ee
Dhanush Reddy <29dhanushreddy@gmail.com>
2022-01-08 10:36:28 +0530

    Made UI Changes

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import "./weather.css";
+import "./styles/weather.css";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -7,29 +7,50 @@ import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 
 export default function weather({ weatherData }) {
+  const date = new Date(weatherData.location.localtime);
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const FormattedDate = `${
+    date.getHours() >= 12 ? date.getHours() - 12 : date.getHours()
+  }:${date.getMinutes()} ${date.getHours() >= 12 ? "PM" : "AM"} - ${
+    days[date.getDay()]
+  }, ${date.getDate()} ${month[date.getMonth()]} '${date.getYear() - 100}`;
   return (
-    <div>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="https://i0.wp.com/shinesolutions.com/wp-content/uploads/2020/03/test-resize.jpg?fit=320%2C240&ssl=1"
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {weatherData.location.name}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Temperature is:{" "}
-              <button className="btn btn-primary">
-                {weatherData.current.temp_c} °C
-              </button>
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+    <div className="box">
+      <div className="temperature">
+        <h1>{weatherData.current.temp_c}°</h1>
+      </div>
+      <div className="city">
+        <div>{weatherData.location.name}</div>
+        <div className="time-details">{FormattedDate}</div>
+      </div>
+      <div className="icons">
+        <div>
+          <img src={weatherData.current.condition.icon} alt="weather-icon" />
+        </div>
+        <div>{weatherData.current.condition.text}</div>
+      </div>
     </div>
   );
 }

7560a90c6536e759631876b32378eb19dfe74a35
Dhanush Reddy <29dhanushreddy@gmail.com>
2022-01-15 18:39:00 +0530

    Adding a Mix of Preloaders

--- src/components/PreLoader.js
+++ src/components/PreLoader.js
@@ -1,9 +1,23 @@
 import React from "react";
 
 export default function PreLoader() {
+  const Loaders = [
+    "lf20_skubx35o",
+    "lf20_zjihqths",
+    "lf20_ajnj9aqh",
+    "lf20_mniampqn",
+    "lf20_z2jkGi",
+    "lf20_fwabdv2k",
+    "lf20_xpJbLw",
+    "lf20_RGolH6",
+    "lf20_odwhwstq",
+    "lf20_ccpdxyfc",
+  ];
   return (
     <lottie-player
-      src="https://assets9.lottiefiles.com/packages/lf20_cgxasnue.json"
+      src={`https://assets9.lottiefiles.com/packages/${
+        Loaders[Math.floor(Math.random() * Loaders.length)]
+      }.json`}
       background="transparent"
       speed="1"
       style={{ width: "300px", height: "300px" }}
